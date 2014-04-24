7e4c527867563945cc9ff125b288deb34955c0d3
dewang wei <dewang.wei@gmail.com>
2014-04-24 15:55:58 +0800

    添加忽略文件 判断远程服务器连接状态判断

--- src/reverse/main.go
+++ src/reverse/main.go
@@ -3,28 +3,30 @@ package main
 import (
   "bufio"
   "encoding/hex"
+  "flag"
   "fmt"
   "net"
   "os"
-  "flag"
 )
 
+var (
+  local_address  string
+  remote_address string
+  remote_timeout int = 30 //远程连接超时时间,单位: 秒
+)
 
-var local_address string
-var remote_address string
-
-func init(){
+func init() {
   flag.Usage = show_usage
   flag.StringVar(&local_address, "l", "localhost:9000", `local listen local ip:port`)
   flag.StringVar(&remote_address, "r", "remote:9000", `remote ip and port  ip:port`)
-  flag.Parse()  
+  flag.IntVar(&remote_timeout, "t", 30,"remote connection timeout: default 30 sec")
+  flag.Parse()
 }
 
-
 func main() {
-  server := initListener(local_address)
-  if server == nil {
-    panic(fmt.Sprintf("couldn't start listening"))
+  server, err := initListener(local_address)
+  if err != nil || server == nil {
+    panic(fmt.Sprintf("ERROR: couldn't start listening"))
   }
   conns := clientConns(server)
   for {
@@ -34,19 +36,23 @@ func main() {
 
 //读取客户端发送来的消息,转发到远端服务器
 func handleConn(c net.Conn) {
-  
+
   cb := readBufferd(bufio.NewReader(c))
   fmt.Printf("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n%v\n", hex.Dump(cb))
 
-  rc := openConnect(remote_address)
+  rc, err := openConnect(remote_address)
   defer closeConnect(rc)
+
+  if rc == nil || err != nil {
+    return
+  }
 
   rc.Write(cb)
   b := bufio.NewReader(rc)
   rrb := readBufferd(b)
   c.Write(rrb)
   fmt.Printf(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n%v\n", hex.Dump(rrb))
-  
+
 }
 
 /*
@@ -59,7 +65,7 @@ func clientConns(listenner net.Listener) chan net.Conn {
     for {
       client, err := listenner.Accept()
       if client == nil {
-        fmt.Printf("couldn't accept: %v", err)
+        fmt.Printf("ERROR: couldn't accept: %v", err)
         continue
       }
       i++
@@ -70,29 +76,36 @@ func clientConns(listenner net.Listener) chan net.Conn {
   return ch
 }
 
-func initListener(addr string) *net.TCPListener {
+func initListener(addr string) (*net.TCPListener, error) {
   s, err := net.ResolveTCPAddr("tcp", addr)
+
   if err != nil {
     panic(fmt.Sprintf("ResolveTCPAddr failed:%v", err))
+    return nil, err
   }
+
   l, err := net.ListenTCP("tcp", s)
+
   if err != nil {
     panic(fmt.Sprintf("can't listen on %s,%v", addr, err))
+    return nil, err
   }
-  return l
+
+  return l, nil
 }
 
-func openConnect(addr string) *net.TCPConn {
+func openConnect(addr string) (*net.TCPConn, error) {
   s, err := net.ResolveTCPAddr("tcp", addr)
   conn, err := net.DialTCP("tcp", nil, s)
   if err != nil {
-    println("Dial failed:", err.Error())
+    println("ERROR: Dial failed:", err.Error())
+    return nil, err
   }
-  return conn
+  return conn, nil
 }
 
 func closeConnect(conn *net.TCPConn) {
-  if &conn != nil {
+  if conn != nil {
     conn.Close()
   }
 }
@@ -105,12 +118,9 @@ func readBufferd(r *bufio.Reader) []byte {
   return buf
 }
 
-
 func show_usage() {
   fmt.Fprintf(os.Stderr,
     "Usage: %s \n",
     os.Args[0])
   flag.PrintDefaults()
 }
-
-
