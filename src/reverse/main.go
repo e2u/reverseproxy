a89f53eac7f725e1dfc29d6ef78f3c5cd5824296
dewang wei <dewang.wei@gmail.com>
2014-04-24 16:58:40 +0800

    添加长连接支持

--- src/reverse/main.go
+++ src/reverse/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-  "bufio"
   "encoding/hex"
   "flag"
   "fmt"
@@ -10,16 +9,16 @@ import (
 )
 
 var (
-  local_address  string
-  remote_address string
-  remote_timeout int = 30 //远程连接超时时间,单位: 秒
+  local_address       string
+  remote_address      string
+  remote_read_timeout int = 30 //远程连接超时时间,单位: 秒
 )
 
 func init() {
   flag.Usage = show_usage
   flag.StringVar(&local_address, "l", "localhost:9000", `local listen local ip:port`)
   flag.StringVar(&remote_address, "r", "remote:9000", `remote ip and port  ip:port`)
-  flag.IntVar(&remote_timeout, "t", 30,"remote connection timeout: default 30 sec")
+  flag.IntVar(&remote_read_timeout, "t", 30, "remote connection timeout: default 30 sec")
   flag.Parse()
 }
 
@@ -35,24 +34,20 @@ func main() {
 }
 
 //读取客户端发送来的消息,转发到远端服务器
-func handleConn(c net.Conn) {
+func handleConn(local_conn net.Conn) {
 
-  cb := readBufferd(bufio.NewReader(c))
-  fmt.Printf("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n%v\n", hex.Dump(cb))
-
-  rc, err := openConnect(remote_address)
-  defer closeConnect(rc)
-
-  if rc == nil || err != nil {
+  remote_conn, err := openConnect(remote_address)
+  //remote_conn.SetReadDeadline(time.Now().Add(time.Duration(remote_read_timeout) * time.Second))
+  remote_conn.SetKeepAlive(true)
+  remote_conn.SetNoDelay(true)
+  if err != nil {
     return
   }
+  //关闭远程连接和本地连接
+  defer remote_conn.Close()
+  defer local_conn.Close()
 
-  rc.Write(cb)
-  b := bufio.NewReader(rc)
-  rrb := readBufferd(b)
-  c.Write(rrb)
-  fmt.Printf(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n%v\n", hex.Dump(rrb))
-
+  Pipe(local_conn, remote_conn)
 }
 
 /*
@@ -104,23 +99,54 @@ func openConnect(addr string) (*net.TCPConn, error) {
   return conn, nil
 }
 
-func closeConnect(conn *net.TCPConn) {
-  if conn != nil {
-    conn.Close()
-  }
-}
-
-func readBufferd(r *bufio.Reader) []byte {
-  r.Peek(1)
-  len := r.Buffered()
-  buf := make([]byte, len)
-  r.Read(buf)
-  return buf
-}
-
 func show_usage() {
   fmt.Fprintf(os.Stderr,
     "Usage: %s \n",
     os.Args[0])
   flag.PrintDefaults()
+}
+
+
+func chanFromConn(conn net.Conn) chan []byte {
+  c := make(chan []byte)
+  go func() {
+    b := make([]byte, 1024)
+    for {
+      n, err := conn.Read(b)
+      if n > 0 {
+        res := make([]byte, n)
+        copy(res, b[:n])
+        c <- res
+      }
+      if err != nil {
+        c <- nil
+        break
+      }
+    }
+  }()
+
+  return c
+}
+
+func Pipe(local net.Conn, remote net.Conn) {
+  local_chan := chanFromConn(local)
+  remote_chan := chanFromConn(remote)
+  for {
+    select {
+    case b1 := <-local_chan:
+      if b1 == nil {
+        return
+      } else {
+        fmt.Printf("LOCAL>>>>>\n%s", hex.Dump(b1))
+        remote.Write(b1)
+      }
+    case b2 := <-remote_chan:
+      if b2 == nil {
+        return
+      } else {
+        fmt.Printf("REMOTE<<<<<\n%s", hex.Dump(b2))
+        local.Write(b2)
+      }
+    }
+  }
 }
